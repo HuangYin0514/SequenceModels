79ff94bcbe0d569d728e5910f6b95937fdad5070
huangyin0611 <1071324110@qq.com>
2019-01-31 23:20:08 +0800

    q1

--- week2/q1/Main.py
+++ week2/q1/Main.py
@@ -9,5 +9,58 @@ from w2v_utils import *
 import numpy as np
 
 if __name__ == '__main__':
+    words, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+    """
+    #test cosine_similarity
+    father = word_to_vec_map["father"]
+    mother = word_to_vec_map["mother"]
+    ball = word_to_vec_map["ball"]
+    crocodile = word_to_vec_map["crocodile"]
+    france = word_to_vec_map["france"]
+    italy = word_to_vec_map["italy"]
+    paris = word_to_vec_map["paris"]
+    rome = word_to_vec_map["rome"]
+    print("cosine_similarity(father, mother) = ", cosine_similarity(father, mother))
+    print("cosine_similarity(ball, crocodile) = ", cosine_similarity(ball, crocodile))
+    print("cosine_similarity(france - paris, rome - italy) = ", cosine_similarity(france - paris, rome - italy))
+    """
+    # test compute_analogy
+    triads_to_try = [('italy', 'italian', 'spain'), ('india', 'delhi', 'japan'), ('man', 'woman', 'boy'),
+                     ('small', 'smaller', 'large')]
+    for triad in triads_to_try:
+        print('{} -> {} ::{} ->{}'.format(*triad, compute_analogy(*triad, word_to_vec_map)))
 
-    pass
+
+    # woman - man
+    g = word_to_vec_map['woman'] - word_to_vec_map['man']
+    print(g)
+
+    print('List of names and their similarities with constructed vector:')
+    # girls and boys name
+    name_list = ['john', 'marie', 'sophie', 'ronaldo', 'priya', 'rahul', 'danielle', 'reza', 'katy', 'yasmin']
+    for w in name_list:
+        print(w, cosine_similarity(word_to_vec_map[w], g))
+
+    print('Other words and their similarities:')
+    word_list = ['lipstick', 'guns', 'science', 'arts', 'literature', 'warrior', 'doctor', 'tree', 'receptionist',
+                 'technology', 'fashion', 'teacher', 'engineer', 'pilot', 'computer', 'singer']
+    for w in word_list:
+        print(w, cosine_similarity(word_to_vec_map[w], g))
+
+
+    # test neutralize
+    e = "receptionist"
+    print("cosine similarity between " + e + " and g, before neutralizing: ",
+          cosine_similarity(word_to_vec_map["receptionist"], g))
+    e_debiased = neutralize("receptionist", g, word_to_vec_map)
+    print("cosine similarity between " + e + " and g, after neutralizing: ", cosine_similarity(e_debiased, g))
+
+    # test equalize
+    print("cosine similarities before equalizing:")
+    print("cosine_similarity(word_to_vec_map[\"man\"], gender) = ", cosine_similarity(word_to_vec_map["man"], g))
+    print("cosine_similarity(word_to_vec_map[\"woman\"], gender) = ", cosine_similarity(word_to_vec_map["woman"], g))
+    print()
+    e1, e2 = equalize(("man", "woman"), g, word_to_vec_map)
+    print("cosine similarities after equalizing:")
+    print("cosine_similarity(e1, gender) = ", cosine_similarity(e1, g))
+    print("cosine_similarity(e2, gender) = ", cosine_similarity(e2, g))
--- week2/q1/MyMethod.py
+++ week2/q1/MyMethod.py
@@ -3,4 +3,116 @@
 # @Author   : HuangYin
 # @FileName : MyMethod.py
 # @Software : PyCharm
+from w2v_utils import *
+import numpy as np
+
+
+def cosine_similarity(u, v):
+    """
+       Cosine similarity reflects the degree of similariy between u and v
+
+       Arguments:
+           u -- a word vector of shape (n,)
+           v -- a word vector of shape (n,)
+
+       Returns:
+           cosine_similarity -- the cosine similarity between u and v defined by the formula above.
+       """
+    distance = 0.0
+    dot = np.dot(u, v)
+    norm_u = np.sqrt(np.sum(np.power(u, 2)))
+    norm_v = np.sqrt(np.sum(np.power(v, 2)))
+    cosine_similarity = dot / np.dot(norm_u, norm_v)
+    return cosine_similarity
+
+
+def compute_analogy(word_a, word_b, word_c, word_to_vec_map):
+    """
+        Performs the word analogy task as explained above: a is to b as c is to ____.
+
+        Arguments:
+        word_a -- a word, string
+        word_b -- a word, string
+        word_c -- a word, string
+        word_to_vec_map -- dictionary that maps words to their corresponding vectors.
+
+        Returns:
+        best_word --  bthe word such that v_b - v_a is close to v_best_word - v_c, as measured by cosine similarity
+        """
+    word_a, word_b, word_c = word_a.lower(), word_b.lower(), word_c.lower()
+    e_a, e_b, e_c = word_to_vec_map[word_a], word_to_vec_map[word_b], word_to_vec_map[word_c]
+
+    words = word_to_vec_map.keys()
+    max_cosine_sim = -100
+    best_word = None
+
+    for w in words:
+        if w in [word_a, word_b, word_c]:
+            continue
+        cosine_sim = cosine_similarity((e_b - e_a), (word_to_vec_map[w] - e_c))
+        if cosine_sim > max_cosine_sim:
+            max_cosine_sim = cosine_sim
+            best_word = w
+
+    return best_word
+
+
+def neutralize(word, g, word_to_vec_map):
+    """
+      Removes the bias of "word" by projecting it on the space orthogonal to the bias axis.
+      This function ensures that gender neutral words are zero in the gender subspace.
+
+      Arguments:
+          word -- string indicating the word to debias
+          g -- numpy-array of shape (50,), corresponding to the bias axis (such as gender)
+          word_to_vec_map -- dictionary mapping words to their corresponding vectors.
+
+      Returns:
+          e_debiased -- neutralized word vector representation of the input "word"
+      """
+    e = word_to_vec_map[word]
+    e_biascomponent = np.divide(np.dot(e, g), np.square(np.linalg.norm(g))) * g
+    e_debiased = e - e_biascomponent
+    return e_debiased
+
+
+def equalize(pair, bias_axis, word_to_vec_map):
+    """
+    通过遵循上图中所描述的均衡方法来消除性别偏差。
+
+    参数：
+        pair -- 要消除性别偏差的词组，比如 ("actress", "actor")
+        bias_axis -- 维度为(50,)，对应于偏置轴（如性别）
+        word_to_vec_map -- 字典类型，单词到GloVe向量的映射
+
+    返回：
+        e_1 -- 第一个词的词向量
+        e_2 -- 第二个词的词向量
+    """
+    # 第1步：获取词向量
+    w1, w2 = pair
+    e_w1, e_w2 = word_to_vec_map[w1], word_to_vec_map[w2]
+
+    # 第2步：计算w1与w2的均值
+    mu = (e_w1 + e_w2) / 2.0
+
+    # 第3步：计算mu在偏置轴与正交轴上的投影
+    mu_B = np.divide(np.dot(mu, bias_axis), np.square(np.linalg.norm(bias_axis))) * bias_axis
+    mu_orth = mu - mu_B
+
+    # 第4步：使用公式7、8计算e_w1B 与 e_w2B
+    e_w1B = np.divide(np.dot(e_w1, bias_axis), np.square(np.linalg.norm(bias_axis))) * bias_axis
+    e_w2B = np.divide(np.dot(e_w2, bias_axis), np.square(np.linalg.norm(bias_axis))) * bias_axis
+
+    # 第5步：根据公式9、10调整e_w1B 与 e_w2B的偏置部分
+    corrected_e_w1B = np.sqrt(np.abs(1 - np.square(np.linalg.norm(mu_orth)))) * np.divide(e_w1B - mu_B,
+                                                                                          np.abs(e_w1 - mu_orth - mu_B))
+    corrected_e_w2B = np.sqrt(np.abs(1 - np.square(np.linalg.norm(mu_orth)))) * np.divide(e_w2B - mu_B,
+                                                                                          np.abs(e_w2 - mu_orth - mu_B))
+
+    # 第6步： 使e1和e2等于它们修正后的投影之和，从而消除偏差
+    e1 = corrected_e_w1B + mu_orth
+    e2 = corrected_e_w2B + mu_orth
+
+    return e1, e2
 
