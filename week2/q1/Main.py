2118cf874dbd0c3e33af27e288d30242632752d9
huangyin0611 <1071324110@qq.com>
2019-01-31 21:30:59 +0800

    init

---
+++ week2/q1/Main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/31 21:29
+# @Author   : HuangYin
+# @FileName : Main.py
+# @Software : PyCharm
+from week2.q1.MyMethod import *
+
+if __name__ == '__main__':
+    pass
---
+++ week2/q1/MyMethod.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/31 21:29
+# @Author   : HuangYin
+# @FileName : MyMethod.py
+# @Software : PyCharm
+
