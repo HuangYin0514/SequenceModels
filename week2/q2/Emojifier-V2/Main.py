441d0a23a53974350dce4a221db4b528a16189c8
huangyin0611 <1071324110@qq.com>
2019-02-01 16:14:26 +0800

    Emojifier-V1

---
+++ week2/q2/Emojifier-V2/Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/31 23:26
+# @Author   : HuangYin
+# @FileName : Main.py
+# @Software : PyCharm
+
+from week2.q2.Method import *
---
+++ week2/q2/Emojifier-V2/Method.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/31 23:26
+# @Author   : HuangYin
+# @FileName : Method.py
+# @Software : PyCharm
+
+
+import numpy as np
+from emo_utils import *
